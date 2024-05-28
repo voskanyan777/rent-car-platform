7dd0b973b6176aeb2ed42e0a0629f160ade5fbba
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-05-28 23:40:57 +0700

    JavaScript file for login page has been changed

--- rent_car/app/static/js/login.js
+++ rent_car/app/static/js/login.js
@@ -1,6 +1,18 @@
 const authForm = document.getElementById('main-form');
 const errorMessage = document.getElementById('error-message');
+const formsField = document.getElementsByTagName('input')
 authForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    errorMessage.style.display = 'flex';
+    let allFieldsFilled = true;
+    Array.from(formsField).forEach((field) => {
+        if (!field.value){
+            allFieldsFilled = false;
+        }
+    });
+    if (!allFieldsFilled){
+        errorMessage.style.display = 'flex';
+    }
+    else{
+        errorMessage.style.display = 'none';
+    }
 })
