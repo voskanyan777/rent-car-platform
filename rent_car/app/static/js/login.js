cb922ef2aa7efe072624270ac5198a32ea71d30f
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-05-31 20:50:26 +0700

    Added message about password mismatch

--- rent_car/app/static/js/login.js
+++ rent_car/app/static/js/login.js
@@ -1,18 +1,25 @@
 const authForm = document.getElementById('main-form');
 const errorMessage = document.getElementById('error-message');
-const formsField = document.getElementsByTagName('input')
+const formsField = document.getElementsByTagName('input');
+const passwordMismatch = document.getElementById('password-mismatch-message');
+
+
 authForm.addEventListener('submit', function (event) {
     event.preventDefault();
     let allFieldsFilled = true;
     Array.from(formsField).forEach((field) => {
-        if (!field.value){
+        if (!field.value) {
             allFieldsFilled = false;
         }
     });
-    if (!allFieldsFilled){
+    if (!allFieldsFilled) {
         errorMessage.style.display = 'flex';
-    }
-    else{
+        passwordMismatch.style.display = 'none';
+    } else if (formsField[2].value != formsField[3].value) {
+        errorMessage.style.display = 'none';
+        passwordMismatch.style.display = 'flex';
+    } else {
+        passwordMismatch.style.display = 'none';
         errorMessage.style.display = 'none';
     }
 })
