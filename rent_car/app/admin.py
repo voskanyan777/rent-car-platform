907e999339e3840c9add5d5e20bf5211bf3eb6a6
voskanyan777 <yura.voskanyan.2003@mail.ru>
2024-06-05 23:58:43 +0700

    Added models in admin panel

--- rent_car/app/admin.py
+++ rent_car/app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CarModel, CarReviewModel, OrdersModel
 
-# Register your models here.
+admin.site.register(CarModel)
+admin.site.register(CarReviewModel)
+admin.site.register(OrdersModel)
